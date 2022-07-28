74a9d5d975138887fa120a8ee47b754a0db70520
nard0g <90727683+nard0g@users.noreply.github.com>
2022-07-28 11:25:40 -0400

    Update fibonacci.js
    Completed fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(fibLength) {
 
+    if (fibLength < 0) {
+        return "OOPS";
+    }
+
+    if (fibLength === 0) {
+        return 0;
+    }
+    
+    let valueA = 0;
+    let valueB = 1;
+    
+    for (i = 1; i < fibLength; i++) {
+        let tempValue = valueB;
+        valueB += valueA;
+        valueA = tempValue;
+    }
+    return valueB;
 };
 
 // Do not edit below this line
