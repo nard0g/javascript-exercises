27cc88b09488c4b3caa29522be1345d2934599b7
nard0g <90727683+nard0g@users.noreply.github.com>
2022-08-02 11:07:21 -0400

    Update findTheOldest.js
    Finished exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let currentOldestPerson;
+    let currentOldestAge = 0;
+    
+    for (let personIndex = 0; personIndex < people.length; personIndex++) {
+        let personChecking = people[personIndex];
+        age = getAge(personChecking);
 
+        if (age > currentOldestAge) {
+            currentOldestPerson = people[personIndex];
+            currentOldestAge = age;
+        }
+    }
+    return currentOldestPerson;
 };
+
+function getAge(personChecking) {
+        if (!personChecking.yearOfDeath) {
+            // If no death date, use current date to find age
+            const date = new Date();
+            return date.getFullYear() - personChecking.yearOfBirth;
+        } else {
+            return personChecking.yearOfDeath - personChecking.yearOfBirth;
+        }
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
